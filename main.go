cef269ea1f7ef32d5b1f3665299be37465bc2ca9
Mikhail Zolenko <mihail.zolenko@gmail.com>
2021-07-01 19:59:15 +0600

    Add telegram bot files

---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type bnResp struct {
+	Price float64 `json:"price,string"`
+	Code  int64   `json:"code"`
+}
+
+type wallet map[string]float64
+
+var db = map[int64]wallet{}
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("1760751596:AAFyiuBK1lma4dIxWwPzVlLmHulbULdImRI")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		command := strings.Split(update.Message.Text, " ")
+
+		switch command[0] {
+		case "ADD":
+			if len(command) != 3 {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "incorrect syntax"))
+				return
+			}
+
+			amount, err := strconv.ParseFloat(command[2], 64)
+			if err != nil {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, err.Error()))
+			}
+
+			if _, ok := db[update.Message.Chat.ID]; !ok {
+				db[update.Message.Chat.ID] = wallet{}
+			}
+
+			db[update.Message.Chat.ID][command[1]] += amount
+
+			balanceText := fmt.Sprintf("%f", db[update.Message.Chat.ID][command[1]])
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, balanceText))
+		case "SUB":
+			if len(command) != 3 {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "incorrect syntax"))
+				return
+			}
+
+			amount, err := strconv.ParseFloat(command[2], 64)
+			if err != nil {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, err.Error()))
+			}
+
+			if _, ok := db[update.Message.Chat.ID]; !ok {
+				continue
+			}
+
+			db[update.Message.Chat.ID][command[1]] -= amount
+
+			balanceText := fmt.Sprintf("%f", db[update.Message.Chat.ID][command[1]])
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, balanceText))
+		case "DEL":
+			if len(command) != 2 {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "the syntax of the command is incorrect"))
+			}
+
+			delete(db[update.Message.Chat.ID], command[1])
+		case "SHOW":
+			msg := ""
+			var sum float64 = 0
+			for key, value := range db[update.Message.Chat.ID] {
+				price, _ := getPrice(key)
+				sum += value * price
+				msg += fmt.Sprintf("%s: %f [%.2f]\n", key, value, value*price)
+			}
+			msg += fmt.Sprintf("Total: %.2f\n", sum)
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, msg))
+		default:
+			bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "command not found"))
+		}
+	}
+}
+
+func getPrice(symbol string) (price float64, err error) {
+	resp, err := http.Get(fmt.Sprintf("https://api.binance.com/api/v3/ticker/price?symbol=%sUSDT", symbol))
+	if err != nil {
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var jsonResp bnResp
+
+	err = json.NewDecoder(resp.Body).Decode(&jsonResp)
+	if err != nil {
+		return
+	}
+
+	if jsonResp.Code != 0 {
+		err = errors.New("wrong symbol")
+	}
+
+	price = jsonResp.Price
+	return
+}
